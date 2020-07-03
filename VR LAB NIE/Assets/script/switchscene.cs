04172925aa1045b7b2717d226308f271f718bd9b
noraishahMusa <67356735+noraishahMusa@users.noreply.github.com>
2020-07-03 15:13:35 +0800

    add button

---
+++ VR LAB NIE/Assets/script/switchscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switchscene : MonoBehaviour
+{
+   public void playGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void home()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
