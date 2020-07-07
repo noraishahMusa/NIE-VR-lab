ae236af01d5575ac27ae9fb145245f6c8d0a7a5d
noraishahMusa <67356735+noraishahMusa@users.noreply.github.com>
2020-07-07 11:02:26 +0800

    added scene

--- VR LAB NIE/Assets/script/switchscene.cs
+++ VR LAB NIE/Assets/script/switchscene.cs
@@ -12,13 +12,20 @@ public class switchscene : MonoBehaviour
     }
     public void playGame2()
     {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+    public void playGame3()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
 
     }
     public void back()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
 }
